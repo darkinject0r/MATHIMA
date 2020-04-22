0054da4eca9b9fa95d04a3b366dad6d5f776efb0
/inject0r <53972067+darkinject0r@users.noreply.github.com>
2020-04-22 05:22:17 +0300

    Add files via upload

---
+++ MariosSite.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<title> Marios Site </title>
+</head>
+<body>
+Hello this is my site!!
+<br><br>
+<?php
+	$a = 'hello';
+	$b = 'world';
+	$c = 'my name is Jim';
+	print $a . ' ' . $b . ' '. $c;
+	
+	$num1 = 10;
+	$num2 = 30;
+	echo '<br>';
+	echo $num1 + $num2;
+	//comments
+	/* multiple comments
+	multiple comments
+	multiple comments
+	multiple comments
+	*/
+	$num = 3.14;
+	$age = 7;
+
+?>
+
+</body>
+</html>

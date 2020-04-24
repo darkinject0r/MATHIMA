7be93066071f31f0c70e44722c85ea76b9d0f0d0
/inject0r <53972067+darkinject0r@users.noreply.github.com>
2020-04-24 21:33:18 +0300

    Add files via upload

---
+++ calc.php
@@ -0,0 +1,15 @@
+<?php
+	echo 'Τιμή Πρώτη:',$_POST['Num1'];
+	echo '<br>';
+	echo 'Τιμή Δεύτερη:',$_POST['Num2'];
+	echo '<br>';
+	if ($_POST['Num1'] >= 0 && $_POST['Num2'] >= 0) {
+		echo 'Άθροισμα = ',$_POST['Num1'] + 	$_POST['Num2'];
+		echo '<br>';
+	}
+	else {
+		echo 'Γινόμενο = ',$_POST['Num1'] * $_POST['Num2'];
+		echo '<br>';
+	}
+
+?>

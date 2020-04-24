c9f4ed17aa6d5a48574c01839bb0446a1ee0df09
/inject0r <53972067+darkinject0r@users.noreply.github.com>
2020-04-24 18:46:08 +0300

    Update Askisi.php

--- Askisi.php
+++ Askisi.php
@@ -2,13 +2,15 @@
 <head>
 <title>Askisi prosthesis</title>
 </head>
-<?php
-$num1 = 1;
-$num2 = 4;
-//KENO DIASTIMA
-echo "<br />";
-echo "<br />";
-print $num1 + $num2;
+<body>
+   <?php
+     $num1 = 1;
+     $num2 = 4;
+     //KENO DIASTIMA
+     echo "<br />";
+     echo "<br />";
+     print $num1 + $num2;
 
- ?>
+   ?>
+</body>
 </html>

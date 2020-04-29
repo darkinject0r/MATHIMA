72a4f5cdff782fcac8b11ee3f3950f546fd39dd3
/inject0r <53972067+darkinject0r@users.noreply.github.com>
2020-04-29 23:13:24 +0300

    Add files via upload

---
+++ Askiseis.php
@@ -0,0 +1,18 @@
+<?php
+      echo "Username:",$_POST["Arithmos1"];
+      echo "<br />";
+      echo "<br />";
+      echo "Password" ,$_POST["Kwdikos"];
+
+  if ($_POST["Username"] = "Mathima" && $_POST["Password"] = "Kwdikos")
+  {
+          echo "Username/Password:",$_POST["Username"].$_POST["Password"];
+          print <br />;
+          print <br />;
+  }
+  else
+  {
+        echo "Wrong Username/password";
+  }
+
+ ?>

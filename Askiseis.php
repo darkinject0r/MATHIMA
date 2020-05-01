44543b9a41b7a6f209318bfb0c4b8ce4284225fa
/inject0r <53972067+darkinject0r@users.noreply.github.com>
2020-05-01 20:31:27 +0300

    Insert tables on the Calculator.html

--- Askiseis.php
+++ Askiseis.php
@@ -1,7 +1,7 @@
 <?php
       echo "Username:",$_POST["Arithmos1"];
-      echo "<br />";
-      echo "<br />";
+      echo "<br>";
+      echo "<br>";
       echo "Password" ,$_POST["Kwdikos"];
 
   if ($_POST["Username"] = "Mathima" && $_POST["Password"] = "Kwdikos")

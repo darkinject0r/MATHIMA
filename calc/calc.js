0b361dd61c566c3be05ec4a5b11bb381a2d9b75e
/inject0r <53972067+darkinject0r@users.noreply.github.com>
2020-05-22 02:30:06 +0300

    ALLAGES.

---
+++ calc/calc.js
@@ -0,0 +1,35 @@
+	function Addition() {
+		var num1,num2;
+		num1 = document.getElementById("number1").value;
+		num2 = document.getElementById("number2").value;
+		num1 = Number(num1);
+		num2 = Number(num2);
+		window.alert(num1+num2);
+	}
+	
+	function Substraction() {
+		var num1,num2;
+		num1 = document.getElementById("number1").value;
+		num2 = document.getElementById("number2").value;
+		num1 = Number(num1);
+		num2 = Number(num2);
+		window.alert(num1-num2);
+	}
+	
+	function Multiplication() {
+		var num1,num2;
+		num1 = document.getElementById("number1").value;
+		num2 = document.getElementById("number2").value;
+		num1 = Number(num1);
+		num2 = Number(num2);
+		window.alert(num1*num2);
+	}
+	
+	function Division() {
+		var num1,num2;
+		num1 = document.getElementById("number1").value;
+		num2 = document.getElementById("number2").value;
+		num1 = Number(num1);
+		num2 = Number(num2);
+		window.alert(num1/num2);
+	}
